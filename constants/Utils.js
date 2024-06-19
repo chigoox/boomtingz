92df9b80a20e606cd06ad97b1ee213240c292d43
chigoox <dikeemmanuel54@gmail.com>
2024-06-19 08:01:08 -0400

    xxx

--- constants/Utils.js
+++ constants/Utils.js
@@ -1,8 +1,4 @@
-import FireClass from "codeby5/Support/FirebaseClass";
-import { firebaseConfig } from "../firebaseConfig";
 
 const { default: UtilClass } = require("codeby5/Support/UtilsClass");
 
-export const { auth, data } = new FireClass(firebaseConfig)
 export const { createArray, formatNumber, getRandNum, filterObject, getRandTextNum } = new UtilClass
-export const { checkCurrentUser, loginWith } = auth
